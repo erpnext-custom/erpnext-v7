f5437f6da2049415634313369847f2ba3ee552af
Kinley Tshering <kxt5258@gmail.com>
2016-12-30 13:04:58 +0600

    asset value modification

--- erpnext/accounts/doctype/asset_modifier_tool/asset_modifier_tool.js
+++ erpnext/accounts/doctype/asset_modifier_tool/asset_modifier_tool.js
@@ -3,6 +3,34 @@
 
 frappe.ui.form.on('Asset Modifier Tool', {
 	refresh: function(frm) {
+			frm.disable_save();
+	},
 
-	}
+	update_asset_value: function(frm) {
+		if(frm.doc.asset && frm.doc.value > 0) {
+			frappe.call({
+				method: "erpnext.accounts.doctype.asset_modifier_tool.asset_modifier_tool.change_value",
+				args: {
+					"asset": frm.doc.asset,
+					"value": frm.doc.value
+				},
+				callback: function(r) {
+					if(r.message == "DONE") {
+						cur_frm.set_df_property("update_asset_value", "read_only", 1)
+						msgprint("An amount of Nu. " + frm.doc.amount + " has been added to " + frm.doc.asset )
+					}
+					else {
+						msgprint(r.message)
+					}
+				}
+			});
+		}
+		else if(frm.doc.amount <= 0) {
+			msgprint("Amount should be greater than 0")
+		}
+		else {
+			msgprint("Fill all fields before submitting")
+		}
+	},
+
 });
--- erpnext/accounts/doctype/asset_modifier_tool/asset_modifier_tool.py
+++ erpnext/accounts/doctype/asset_modifier_tool/asset_modifier_tool.py
@@ -8,3 +8,50 @@ from frappe.model.document import Document
 
 class AssetModifierTool(Document):
 	pass
+
+##
+# Method call from client to perform reappropriation
+##
+@frappe.whitelist()
+def change_value(asset=None, value=None):
+	if(asset and value):
+		asset_obj = = frappe.get_doc("Asset", asset)
+
+		if asset_obj:
+			frappe.throw(str(asset_obj.name) + " " + str(asset_obj.gross_purchase_amount) + " " + str(asset_obj.asset_account) + " " + str(asset_obj.schedules)  )
+			#Deduct in the From Account and Cost Center
+			"""from_budget_account = frappe.get_doc("Budget Account", from_account[0].name)
+			sent = flt(from_budget_account.budget_sent) + flt(amount)
+			total = flt(from_budget_account.budget_amount) - flt(amount)
+			from_budget_account.db_set("budget_sent", sent)
+			from_budget_account.db_set("budget_amount", total)
+
+			#Add in the To Account and Cost Center
+			to_budget_account = frappe.get_doc("Budget Account", to_account[0].name)
+			received = flt(to_budget_account.budget_received) + flt(amount)
+			total = flt(to_budget_account.budget_amount) + flt(amount)
+			to_budget_account.db_set("budget_received", received)
+			to_budget_account.db_set("budget_amount", total)
+
+			#Add the reappropriation details for record
+			app_details = frappe.new_doc("Reappropriation Details")
+			app_details.from_cost_center = from_cc
+			app_details.to_cost_center = to_cc
+			app_details.from_account = from_acc
+			app_details.to_account = to_acc
+			app_details.amount = amount
+			app_details.appropriation_on = nowdate()
+			app_details.submit() """
+
+			return "DONE"
+
+		else:
+			return "Invalid asset code"
+
+	elif not asset:
+		return "Invalid asset code"
+
+	elif not value:
+		return "Invalid asset value"
+	else:
+		return "Sorry, something happened. Please try again"
