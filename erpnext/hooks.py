5d50771d99e6e379e8ff1f0fa9387a80baa27832
kxt5258 <kxt5258@gmail.com>
2017-06-21 12:15:32 +0600

    property setter fixture export

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -43,7 +43,7 @@ email_append_to = ["Job Applicant", "Opportunity", "Issue"]
 
 calendars = ["Task", "Production Order", "Leave Application", "Sales Order", "Holiday List"]
 
-fixtures = ["Web Form", "Custom Field"]
+fixtures = ["Web Form", "Custom Field", "Property Setter"]
 
 website_generators = ["Item Group", "Item", "Sales Partner", "Job Opening"]
 
