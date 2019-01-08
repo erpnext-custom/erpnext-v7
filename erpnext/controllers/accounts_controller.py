7e6416ee701a4ae8b1a674e7c41261b7eb77fd35
Kinley Tshering <kxt5258@gmail.com>
2019-01-08 18:35:24 +0600

    prevent invoice without advance adjustment

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -348,7 +348,7 @@ class AccountsController(TransactionBase):
 			advance_entries_against_si = [d.reference_name for d in self.get("advances")]
 			for d in advance_entries:
 				if not advance_entries_against_si or d.reference_name not in advance_entries_against_si:
-					frappe.msgprint(_("Payment Entry {0} is linked against Order {1}, check if it should be pulled as advance in this invoice.")
+					frappe.throw(_("Payment Entry {0} is linked against Order {1}, check if it should be pulled as advance in this invoice.")
 						.format(d.reference_name, d.against_order))
 
 	def update_against_document_in_jv(self):
