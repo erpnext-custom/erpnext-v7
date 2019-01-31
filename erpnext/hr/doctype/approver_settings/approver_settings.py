a3c7ee66c10a6c2938778abaf3ff2212b256ad18
Kinley Tshering <kxt5258@gmail.com>
2019-01-31 10:14:04 +0600

    Added option to get final_approvers for child cost centers aswel

--- erpnext/hr/doctype/approver_settings/approver_settings.py
+++ erpnext/hr/doctype/approver_settings/approver_settings.py
@@ -14,6 +14,15 @@ class ApproverSettings(Document):
 def get_final_approver(branch):
 	if not branch:
 		frappe.throw("Branch not passed")
+
+	# Ver 3.0.190131 Begins, following code added by SHIV
+	# For head office divisions, final approvers are set on child cost centers
+	approver = frappe.db.get_value("Approver Settings", {"cost_center": get_branch_cc(branch)}, "user_id")
+
+	if approver:
+		return approver
+	# Ver 3.0.190131 Ends
+
 	cc = frappe.db.get_value("Cost Center", get_branch_cc(branch), "parent_cost_center")
 	approver = frappe.db.get_value("Approver Settings", {"cost_center": cc}, "user_id")
 	if not approver:
