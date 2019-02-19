18924b2efe2051a108c21891ca11899d6fb534fb
Kinley Tshering <kxt5258@gmail.com>
2019-02-19 09:52:46 +0600

    SWS application

--- erpnext/hr/doctype/sws_application/sws_application.py
+++ erpnext/hr/doctype/sws_application/sws_application.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
-
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
@@ -10,11 +9,11 @@ from frappe.utils import flt
 class SWSApplication(Document):
 	def get_status(self):
 		if self.workflow_state =="Verified By Supervisor":
-			self.verified =1
+			self.verified = 1
 		if self.workflow_state =="Rejected":
-			self.approval_status =="Rejected"
+			self.approval_status = "Rejected"
 		if self.workflow_state =="Approved":
-			self.approval_status =="Approved"
+			self.approval_status = "Approved"
 	
 	def validate(self):
 		self.get_status()
@@ -36,14 +35,16 @@ class SWSApplication(Document):
 
 	def validate_dead(self):
 		for a in self.items:
-			if a.dead:
+			if a.dead == 1:
 				frappe.throw("The dependent is marked dead. Please contact HR Section")
+
+	def before_submit(self):
+		self.get_status()
 
 	def on_submit(self):
 		if self.total_amount <= 0:
 			frappe.throw("Total Amount cannot be 0 or less")
 		self.verify_approvals()
-		self.get_status()
 		self.update_status()
 		self.post_sws_entry()
 	
@@ -73,9 +74,17 @@ class SWSApplication(Document):
 		doc.debit = self.total_amount
 		doc.submit()
 
+	def before_cancel(self):
+		self.reset_status()
+
 	def on_cancel(self):
 		self.update_status()
 		self.delete_sws_entry()
+
+	def reset_status(self):
+		self.workflow_state = "Cancelled"
+		self.verified = 0
+		self.approval_status = None
 
 	def delete_sws_entry(self):
 		frappe.db.sql("delete from `tabSWS Entry` where ref_doc = %s", self.name)
