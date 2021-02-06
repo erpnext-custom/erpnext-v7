f08301c8aaff5443f878aec4bd0c79155bcef4a5
kalsetti <sivasankar.k2003@gmail.com>
2021-02-06 18:52:52 +0600

    Phase-II migrations 2021/02/06

--- erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.js
+++ erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.js
@@ -25,16 +25,50 @@ frappe.ui.form.on('CRM Branch Setting', {
 				filters: {'branch': frm.doc.branch}
 			}
 		};
+		frm.fields_dict['items'].grid.get_field('item').get_query = function() {
+			return {
+				query: "erpnext.crm.doctype.crm_branch_setting.crm_branch_setting.get_items",
+				filters:{"product_category": frm.doc.product_category}       
+			}
+		}
 	},
 	refresh: function(frm) {
-		//enable_disable(frm);
+		enable_disable(frm);
+		set_transport_mode(frm);
 	},
 	has_common_pool: function(frm){
-		//enable_disable(frm);
+		enable_disable(frm);
 	},
+	branch: function(frm){
+		cur_frm.set_value("default_warehouse", null);
+	},
+	product_category: function(frm){
+		set_transport_mode(frm);
+	}
 });
 
 var enable_disable = function(frm){
 	cur_frm.toggle_reqd("lead_time", frm.doc.has_common_pool);
 }
 
+var set_transport_mode = function(frm){
+	if(frm.doc.product_category){
+		frappe.call({
+			method: 'frappe.client.get_value',
+			args: {
+				doctype: 'Product Category',
+				filters: {
+					'name': frm.doc.product_category
+				},
+				fieldname: '*'
+			},
+			callback: function(r){
+				if(r.message){
+					cur_frm.toggle_display("sb_transport_settings", r.message.transport_mode_required);
+				}
+			}
+		});
+	} else {
+		cur_frm.toggle_display("sb_transport_settings", 0);
+	}
+}
--- erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.py
+++ erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.py
@@ -9,16 +9,28 @@ from frappe.utils import cint, flt
 from frappe.model.document import Document
 
 class CRMBranchSetting(Document):
+	#Autonaming of doc - added by Kinley Dorji 09/11/2020
+	def autoname(self):
+		self.name = self.branch+" ("+self.product_category+")"
+
 	def validate(self):
 		self.validate_defaults()
+		# self.validate_transportation()
 		self.update_item_details()
+	
+	# following method is created by SHIV on 2020/11/23 to accommodate Phase-II changes
+	# def validate_transportation(self):
+	# 	if not cint(self.has_common_pool) and not cint(self.allow_self_owned_transport) and not cint(self.allow_other_transport):
+	# 		frappe.throw(_("Transport mode should be Common Pool/Self Owned Transport/Others"))
 
+	#Edited to allow multiple branches but with unique product category - Kinley Dorji 09/11/2020 
 	def validate_defaults(self):
-		if frappe.db.exists("CRM Branch Setting", {"name": ("!=", self.name), "branch": self.branch}): 
+		if frappe.db.exists("CRM Branch Setting", {"name": ("!=", self.name), "branch": self.branch, "product_category": self.product_category}): 
 			old_name = frappe.db.get_value("CRM Branch Setting", {"name": ("!=", self.name), "branch": self.branch}, "name")
-			frappe.throw(_("Branch already exists as <b>{0}</b>").format(old_name))
+			frappe.throw(_("Branch already exists as <b>{0}</b> with Product Category as <b>{1}</b>").format(old_name, self.product_category))
 		elif cint(self.has_common_pool) and not cint(self.lead_time): 
 			frappe.throw(_("Lead Time is mandatory for branches with common pool facility"))
+	
 
 	def update_item_details(self):
 		dup = {}
@@ -82,4 +94,39 @@ class CRMBranchSetting(Document):
 					frappe.throw(_("Row#{0}: Limit/Year cannot be less than Limit/Day").format(i.idx))
 			else:
 				i.has_limit = 0
+
+@frappe.whitelist()
+def get_items(doctype=None, txt=None, searchfield=None, start=None, page_len=None, filters=None):
+        from erpnext.controllers.queries import get_match_cond
+
+	if not filters.get("product_category"):
+		frappe.throw(_("Please select a Product Category first"))
+
+	return frappe.db.sql("""
+			select name, item_name, stock_uom
+			from `tabItem` i
+			where exists(select 1
+				from `tabProduct Category Item` pci
+				where pci.parent = '{product_category}'
+				and pci.item_sub_group = i.item_sub_group)
+			and ({key} like %(txt)s
+				or item_name like %(txt)s
+				or stock_uom like %(txt)s)
+			{mcond}
+			order by
+				if(locate(%(_txt)s, name), locate(%(_txt)s, name), 99999),
+				if(locate(%(_txt)s, item_name), locate(%(_txt)s, item_name), 99999),
+				if(locate(%(_txt)s, stock_uom), locate(%(_txt)s, stock_uom), 99999),
+				idx desc,
+				name, item_name, stock_uom
+			limit %(start)s, %(page_len)s""".format(**{
+				'key': searchfield,
+				'mcond': get_match_cond(doctype),
+				'product_category': filters.get("product_category")
+			}), {
+				'txt': "%%%s%%" % txt,
+				'_txt': txt.replace("%", ""),
+				'start': start,
+				'page_len': page_len
+	})
 
