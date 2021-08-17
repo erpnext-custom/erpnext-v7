2beae0627fb7d119cb810bb7878e213d95afe101
thukten <thukday@gmail.com>
2021-08-17 04:43:57 -0400

    vehicle request change

--- erpnext/maintenance/doctype/vehicle_request/vehicle_request.js
+++ erpnext/maintenance/doctype/vehicle_request/vehicle_request.js
@@ -3,24 +3,23 @@
 
 cur_frm.add_fetch('employee', 'branch', 'branch');
 frappe.ui.form.on('Vehicle Request', {
-	refresh: function (frm) {
-		if(frm.doc.workflow_state == "Waiting MTO Approval" || frm.doc.workflow_state == "Waiting DG Approval"){
-			console.log("SONAM HERE")
-			cur_frm.toggle_display("section_break_003", frappe.user.has_role(["ADM User","CEO"]));
-		}else if(frm.doc.workflow_state == "Approved"){
-			cur_frm.toggle_display("section_break_003", 1);
-		}else{
-			cur_frm.toggle_display("section_break_003", 0);
-		}
-	},
-	setup: function (frm) {
-		frm.get_field('items').grid.editable_fields = [
-			{ fieldname: 'employee', columns: 2 },
-			{ fieldname: 'employee_name', columns: 2 },
-			{ fieldname: 'designation', columns: 2 },
-			{ fieldname: 'division', columns: 3 },
-		];
-	},
+    refresh: function (frm) {
+        if(frm.doc.workflow_state == "Waiting MTO Approval" || frm.doc.workflow_state == "Waiting DG Approval"){
+            cur_frm.toggle_display("section_break_003", frappe.user.has_role(["ADM User","CEO"]));
+        }else if(frm.doc.workflow_state == "Approved"){
+            cur_frm.toggle_display("section_break_003", 1);
+        }else{
+            cur_frm.toggle_display("section_break_003", 0);
+        }
+    },
+    setup: function (frm) {
+        frm.get_field('items').grid.editable_fields = [
+            { fieldname: 'employee', columns: 2 },
+            { fieldname: 'employee_name', columns: 2 },
+            { fieldname: 'designation', columns: 2 },
+            { fieldname: 'division', columns: 3 },
+        ];
+    },
 })
 
 //Returns own equipments
@@ -31,6 +30,6 @@ cur_frm.fields_dict.equipment.get_query = function (doc) {}
         var d = locals[cdt][cdn];
         return {
             query: "erpnext.controllers.queries.employee_query",
-			filters: {'branch': frm.branch}
+            filters: {'branch': frm.branch}
         }
 }*/
