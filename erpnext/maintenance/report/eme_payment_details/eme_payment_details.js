d5a308247085068880e7baf7ddc326b65a383861
Kinley Tshering <kxt5258@gmail.com>
2021-08-30 07:05:26 +0600

    EME Payment Details Report

---
+++ erpnext/maintenance/report/eme_payment_details/eme_payment_details.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["EME Payment Details"] = {
+	"filters": [
+		{
+			"fieldname":"name",
+			"label":__("Reference"),
+			"fieldtype":"Link",
+			"options":"EME Payment",
+			"reqd":1
+		}
+	]
+}
