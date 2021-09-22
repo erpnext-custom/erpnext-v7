fa8bcf121d42b0cd34ce2e8575cfcadbe005e6b6
kalisetti <sivasankar.k2003@gmail.com>
2021-09-22 19:32:33 +0600

    Change in bank payment for payment entry

--- erpnext/accounts/doctype/bank_payment/bank_payment.py
+++ erpnext/accounts/doctype/bank_payment/bank_payment.py
@@ -515,7 +515,7 @@ class BankPayment(Document):
 			branch = self.branch,
 			cond = cond), as_dict=True)
 
-	def get_payment_entry(self):
+    def get_payment_entry(self):
 		cond = ""
 		if self.transaction_no:
 			cond = 'AND pe.name = "{}"'.format(self.transaction_no)
@@ -524,8 +524,8 @@ class BankPayment(Document):
 	  
 		return frappe.db.sql("""SELECT "Payment Entry" transaction_type, pe.name transaction_id, 
 						pe.name transaction_reference, pe.posting_date transaction_date, 
-						pe.party as supplier, pe.party as beneficiary_name,
-						s.bank_name_new as bank_name, s.bank_branch, s.bank_account_type, s.account_number as bank_account_no,
+						pe.party as supplier, pe.party as beneficiary_name, 
+						s.bank_name_new as bank_name, s.bank_branch, fib.financial_system_code, s.bank_account_type, s.account_number as bank_account_no,
 						round((pe.paid_amount + (select ifnull(sum(ped.amount),0)
 											from `tabPayment Entry Deduction` ped
 											where ped.parent = pe.name
@@ -534,13 +534,14 @@ class BankPayment(Document):
 						(CASE WHEN s.bank_name_new = "INR" THEN s.inr_bank_code ELSE NULL END) inr_bank_code,
 						(CASE WHEN s.bank_name_new = "INR" THEN s.inr_purpose_code ELSE NULL END) inr_purpose_code,
 						"Draft" status
-					FROM `tabPayment Entry` pe, `tabSupplier` s
+					FROM `tabPayment Entry` pe
+					JOIN `tabSupplier` s ON s.name = pe.party
+					LEFT JOIN `tabFinancial Institution Branch` fib ON fib.name = s.bank_branch
 					WHERE pe.branch = "{branch}" 
 					{cond}
 					AND pe.docstatus = 1
 					AND pe.party_type = 'Supplier'
 					AND pe.party IS NOT NULL
-					AND s.name = pe.party
 					AND IFNULL(pe.paid_amount,0) > 0
 					AND NOT EXISTS(select 1
 						FROM `tabBank Payment Item` bpi
@@ -689,12 +690,13 @@ def process_one_to_one_payment(doc, publish_progress=True):
 			BnfcryName = i.beneficiary_name
 			if i.employee:
 				bnf_acc_type = frappe.db.get_value("Employee", i.employee, "bank_account_type")
+				BfscCode = str(frappe.db.get_value("Financial Institution Branch", frappe.db.get_value("Employee", i.employee, "bank_branch"), "financial_system_code"))
 			else:
 				bnf_acc_type = frappe.db.get_value("Supplier", i.supplier, "bank_account_type")
+				BfscCode = str(frappe.db.get_value("Financial Institution Branch", frappe.db.get_value("Supplier", i.supplier, "bank_branch"), "financial_system_code"))
 			BnfcryAcctTyp = bnf_acc_type
 			BnfcryRmrk = str(doc.remarks)
 			RemitterName = doc.company
-			BfscCode = frappe.db.get_value("Financial Institution Branch", frappe.db.get_value("Supplier", i.supplier, "bank_branch"), "financial_system_code")
 			RemitterAcctType = frappe.db.get_value("Account", doc.paid_from, "bank_account_type")
 			result = inter_payment(Amt, PayeeAcctNum, BnfcryAcct, BnfcryName, BnfcryAcctTyp, BnfcryRmrk, RemitterName, BfscCode, RemitterAcctType, PEMSRefNum)
 		if result['status'] == "Success":
