5197d032a3befc458b74b80505950daf0f1cb3f8
Kinley Tshering <kxt5258@gmail.com>
2021-10-01 19:53:44 +0600

    Production Summary

---
+++ erpnext/production/report/production_summary/production_summary.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Production Summary"] = {
+	"filters": [
+		{
+			"fieldname": "cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "Link",
+			"options": "Cost Center"
+		},
+		{
+			"fieldname": "branch",
+			"label": __("Branch"),
+			"fieldtype": "Link",
+			"options": "Branch"
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.month_start()
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default":frappe.datetime.get_today()
+		},
+		{
+			"fieldname": "warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname": "to_warehouse",
+			"label": __("Transfer to Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname": "location",
+			"label": __("Location"),
+			"fieldtype": "Link",
+			"options": "Location"
+		},
+		{
+			"fieldname": "periodicity",
+			"label": __("Periodicity"),
+			"fieldtype": "Select",
+			"options": [
+				{ "value": "Daily", "label": __("Daily") },
+				{ "value": "Monthly", "label": __("Monthly") },
+				{ "value": "Quarterly", "label": __("Quarterly") },
+				{ "value": "Half-Yearly", "label": __("Half-Yearly") },
+				{ "value": "Yearly", "label": __("Yearly") }
+			],
+			"default": "Daily",
+			"reqd": 1,
+			"on_change":(query_report)=>{
+				var from_date = query_report.filters_by_name["from_date"];
+				var to_date = query_report.filters_by_name["to_date"];
+				if  (query_report.get_values().periodicity != 'Daily'){
+					from_date.toggle(false)
+					to_date.toggle(false)
+				}else{
+					from_date.toggle(true)
+					to_date.toggle(true)
+				}
+				query_report.refresh()
+			}
+		},
+		{
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1
+		}
+	]
+}
