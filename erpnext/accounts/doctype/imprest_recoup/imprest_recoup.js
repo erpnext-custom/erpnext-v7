c09a23244fb1edf69bdb3faec0f0f8bbe71852d1
Jai Raj Rai <jairajrai1@gmail.com>
2021-10-13 15:30:49 +0600

    updated filter posting date in view ledger

--- erpnext/accounts/doctype/imprest_recoup/imprest_recoup.js
+++ erpnext/accounts/doctype/imprest_recoup/imprest_recoup.js
@@ -24,6 +24,8 @@ frappe.ui.form.on('Imprest Recoup', {
 			frm.add_custom_button(__('Accounting Ledger'), function () {
 				frappe.route_options = {
 					voucher_no: frm.doc.name,
+					from_date: frm.doc.posting_date,
+					to_date: frm.doc.posting_date,
 					company: frm.doc.company,
 					group_by_voucher: false
 				};
