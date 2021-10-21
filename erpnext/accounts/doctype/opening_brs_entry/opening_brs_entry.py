d1be844b0554a6ae7f868254b2aebc59f7118e33
chophel9808 <sonamax809@gmail.com>
2021-10-21 16:24:07 +0600

    BRS updates, rrco permission to cancle and delete and removed branch filter from tds_certificate

--- erpnext/accounts/doctype/opening_brs_entry/opening_brs_entry.py
+++ erpnext/accounts/doctype/opening_brs_entry/opening_brs_entry.py
@@ -12,5 +12,10 @@ class OpeningBRSEntry(Document):
 
 	def validate_amount(self):
 		for d in self.details:
-			if(d.credit_amount > 0 and d.debit_amount > 0):
-				frappe.throw(_("At Row {0} either Credit Amount should be 0 or Debit Amount should be 0 for party: {1}").format(d.idx, d.party))
+			# if(not d.credit_amount.isdigit()):
+			# 	frappe.throw(_("At Row {0} credit amount should not contain any letters for party {1}").format(d.idx, d.party))
+			# elif(not d.debit_amount.isdigit()):
+			# 	frappe.throw(_("At Row {0}  debit amount should not contain any letters for party {1}").format(d.idx, d.party))
+			# else:
+			if(float(d.credit_amount) > 0 and float(d.debit_amount) > 0):
+				frappe.throw(_("At Row {0} either Credit Amount should be 0 or Debit Amount should be 0 for party {1}").format(d.idx, d.party))
--- erpnext/accounts/report/tds_certificate/tds_certificate.js
+++ erpnext/accounts/report/tds_certificate/tds_certificate.js
@@ -2,68 +2,61 @@
 // License: GNU General Public License v3. See license.txt
 
 frappe.query_reports["TDS Certificate"] = {
-	"filters": [
-		{
-			"fieldname": "branch",
-			"label": __("Branch"),
-			"fieldtype": "Link",
-			"options": "Branch",
-			"reqd": 1
-		},
-		{
-			"fieldname": "fiscal_year",
-			"label": __("Fiscal Year"),
-			"fieldtype": "Link",
-			"options": "Fiscal Year",
-			"default": frappe.defaults.get_user_default("fiscal_year"),
-			"reqd": 1,
-			"on_change": function(query_report) {
-				var fiscal_year = query_report.get_values().fiscal_year;
-				if (!fiscal_year) {
-					return;
-				}
-				frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
-					var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
-					query_report.filters_by_name.from_date.set_input(fy.year_start_date);
-					query_report.filters_by_name.to_date.set_input(fy.year_end_date);
-					query_report.trigger_refresh();
-				});
-			}
-		},
-		{
-			"fieldname": "from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.defaults.get_user_default("year_start_date"),
-		},
-		{
-			"fieldname": "to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": frappe.defaults.get_user_default("year_end_date"),
-		},
-		{
-			"fieldname": "vendor_name",
-			"label": __("Vendor Name"),
-			"fieldtype": "Link",
-			"options": "Supplier",
-			"on_change": function(query_report) {
-				var vendor = query_report.get_values().vendor_name;
-				if (!vendor) {
-					return;
-				}
-				frappe.model.with_doc("Supplier", vendor, function(r) {
-					var fy = frappe.model.get_doc("Supplier", vendor);
-					query_report.filters_by_name.vendor_tpn_no.set_input(fy.vendor_tpn_no);
-					query_report.trigger_refresh();
-				});
-			}
-		},
-		{
-			"fieldname": "vendor_tpn_no",
-			"label": __("Vendor TPN Number"),
-			"fieldtype": "Data",
-			"read_only": 1
-		},
-	],
+    "filters": [
+        {
+            "fieldname": "fiscal_year",
+            "label": __("Fiscal Year"),
+            "fieldtype": "Link",
+            "options": "Fiscal Year",
+            "default": frappe.defaults.get_user_default("fiscal_year"),
+            "reqd": 1,
+            "on_change": function(query_report) {
+                var fiscal_year = query_report.get_values().fiscal_year;
+                if (!fiscal_year) {
+                    return;
+                }
+                frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
+                    var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
+                    query_report.filters_by_name.from_date.set_input(fy.year_start_date);
+                    query_report.filters_by_name.to_date.set_input(fy.year_end_date);
+                    query_report.trigger_refresh();
+                });
+            }
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.defaults.get_user_default("year_start_date"),
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.defaults.get_user_default("year_end_date"),
+        },
+        {
+            "fieldname": "vendor_name",
+            "label": __("Vendor Name"),
+            "fieldtype": "Link",
+            "options": "Supplier",
+            "on_change": function(query_report) {
+                var vendor = query_report.get_values().vendor_name;
+                if (!vendor) {
+                    return;
+                }
+                frappe.model.with_doc("Supplier", vendor, function(r) {
+                    var fy = frappe.model.get_doc("Supplier", vendor);
+                    query_report.filters_by_name.vendor_tpn_no.set_input(fy.vendor_tpn_no);
+                    query_report.trigger_refresh();
+                });
+            }
+        },
+        {
+            "fieldname": "vendor_tpn_no",
+            "label": __("Vendor TPN Number"),
+            "fieldtype": "Data",
+            "read_only": 1
+        },
+    ],
 }
