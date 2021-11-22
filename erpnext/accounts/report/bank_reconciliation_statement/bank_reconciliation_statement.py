6bf91578a8a33da65944548f5b46772439bbc88d
Jai Raj Rai <jairajrai1@gmail.com>
2021-11-22 16:48:30 +0600

    Bank Reconciliation Statement

--- erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
+++ erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
@@ -196,8 +196,8 @@ def get_entries(filters):
 		select
 			"Mechanical Payment" as payment_document, name as payment_entry,
 			cheque_no as reference_no, cheque_date as ref_date,
-			IF(payment_for = "Job Card", net_amount, 0) as credit, 
-			IF(payment_for != "Job Card", net_amount, 0) as debit,
+			net_amount as credit, 
+			0 as debit,
 			posting_date, customer as against_account, clearance_date, 'BTN' as account_currency
 		from `tabMechanical Payment`
 		where  %(account)s IN (income_account, expense_account, outgoing_account)
