e1f1b61c4e9e6cfa85d85eee6369b22ef4861038
Jai Raj Rai <jairajrai1@gmail.com>
2021-12-21 17:04:37 +0600

    Stock controller

--- erpnext/controllers/stock_controller.py
+++ erpnext/controllers/stock_controller.py
@@ -43,10 +43,6 @@ class StockController(AccountsController):
 			warehouse_account = get_warehouse_account()
 		sle_map = self.get_stock_ledger_details()
 		voucher_details = self.get_voucher_details(default_expense_account, default_cost_center, sle_map, default_business_activity)
-		
-		# frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(voucher_details)))
-		# frappe.throw("")
-
 		gl_list = []
 		warehouse_with_no_account = []
 		for detail in voucher_details:
@@ -389,8 +385,7 @@ class StockController(AccountsController):
 		else:
 			is_expense_account = frappe.db.get_value("Account",item.get("expense_account"), "report_type")=="Profit and Loss"
 			if self.doctype not in ("Purchase Receipt", "Purchase Invoice", "Stock Reconciliation", "Stock Entry") and not is_expense_account:
-				# frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(item)))
-
+				frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(item)))
 				frappe.throw(_("Expense / Difference account ({0}) must be a 'Profit or Loss' account")
 					.format(item.get("expense_account")))
 			if is_expense_account and not item.get("cost_center"):
