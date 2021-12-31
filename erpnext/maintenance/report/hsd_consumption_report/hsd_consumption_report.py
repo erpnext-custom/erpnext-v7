5e48b0e124f1ed92f4e84b7fcab19bec897dc03f
chophel9808 <sonamax809@gmail.com>
2021-12-31 16:58:55 +0600

    Made report to be in date range that is applied in filter

--- erpnext/maintenance/report/hsd_consumption_report/hsd_consumption_report.py
+++ erpnext/maintenance/report/hsd_consumption_report/hsd_consumption_report.py
@@ -18,71 +18,85 @@ def execute(filters=None):
 def get_data(filters):
 	data = []
 	if filters.get("branch"):
-                branch_cond =  " and eh.branch = \'" + str(filters.branch) + "\'"
-        else:
-                branch_cond = " and eh.branch = eh.branch" 
+		branch_cond =  " and eh.branch = \'" + str(filters.branch) + "\'"
+	else:
+		branch_cond = " and eh.branch = eh.branch" 
 
 	if filters.get("from_date") and filters.get("to_date"):
-                vl_date = " and vl.from_date between \'" + str(filters.from_date) + "\' and \'"+ str(filters.to_date) + "\' and vl.to_date between \'" + str(filters.from_date) + "\' and \'"+ str(filters.to_date) + "\'"
-
+		vl_date = " and vl.from_date between \'" + str(filters.from_date) + "\' and \'"+ str(filters.to_date) + "\' and vl.to_date between \'" + str(filters.from_date) + "\' and \'"+ str(filters.to_date) + "\'"
 		eh_cond = " and (('{0}' between eh.from_date and ifnull(eh.to_date, now())) or ('{1}' between eh.from_date and ifnull(eh.to_date, now())))".format(filters.get("from_date"), filters.get("to_date"))
-        if filters.get("not_cdcl"):
-               not_cdcll = " and e.not_cdcl = 0"
+	
+	if filters.get("not_cdcl"):
+		not_cdcll = " and e.not_cdcl = 0"
 	else:
-			   not_cdcll = ""
+		not_cdcll = ""
 
-        if filters.get("include_disabled"):
-                dis = " "
-        else:
-                dis = " and e.is_disabled = 0"
+	if filters.get("include_disabled"):
+		dis = " "
+	else:
+		dis = " and e.is_disabled = 0"
+
 	if filters.get("category"):
 		cat = " and e.equipment_category = '{0}'".format(filters.get("category"))
-	else: cat = " and e.equipment_category = e.equipment_category"
+	else: 
+		cat = " and e.equipment_category = e.equipment_category"
 
-       	#query += " GROUP BY e.equipment_number, eh.branch "
+	#query += " GROUP BY e.equipment_number, eh.branch "
 	datas = ''
 	query = ''
 	date = str(filters.from_date).split("-")
 	from_date = date[0]+"-"+date[1]+"-"+"01"
 	to_date = get_last_day(from_date)
 	equipments = frappe.db.sql("""
-                        select e.name as name, eh.branch as branch, e.hsd_type, e.equipment_number as equipment_number, 
-                                        e.equipment_type as equipment_type, e.hsd_type, e.equipment_model as equipment_model
-                                        from `tabEquipment` e, `tabEquipment History` eh 
-                                        where eh.parent = e.name
-                                        {0} {1} {2} {3} {4}
-                                         group by eh.branch, eh.parent order by eh.branch, eh.parent
-                                """.format(not_cdcll, branch_cond, dis, eh_cond, cat), as_dict = True, debug =1)
+		select 
+			e.name as name, eh.branch as branch, e.hsd_type, e.equipment_number as equipment_number, 
+			e.equipment_type as equipment_type, e.hsd_type, e.equipment_model as equipment_model
+		from `tabEquipment` e, `tabEquipment History` eh 
+		where eh.parent = e.name
+		{0} {1} {2} {3} {4}
+		group by eh.branch, eh.parent order by eh.branch, eh.parent
+		""".format(not_cdcll, branch_cond, dis, eh_cond, cat), as_dict = True, debug =1)
 	for eq in equipments:
-		query = """ select '{0}' as name, '{1}' as ty, '{2}' as no, '{3}' as br, vl.branch, ifnull(MIN(vl.initial_km),0)  AS mink, ifnull(MAX(vl.final_km),0) AS maxk, ifnull(MIN(vl.initial_hour),0) as minh, 
-                	ifnull(MAX(vl.final_hour),0) as maxh, ifnull(vl.consumption_km,0) as ckm, ifnull(vl.consumption_hours,0) as ch,
-                	(select ifnull((sum(pol.qty*pol.rate)/sum(pol.qty)),0) from tabPOL pol 
-                        where pol.equipment_number = '{2}' and pol.posting_date between '{7}' and '{8}') as rate,
-                	(select em.tank_capacity from  `tabEquipment Model` em where em.name = '{5}') as cap, 
-                	CASE
-			WHEN vl.ys_km THEN vl.ys_km
-			else 0
-			end as yskm,
-			CASE
-			WHEN vl.ys_hours THEN vl.ys_hours
-			else 0
-			end as yshour,
-			ifnull(sum(vl.distance_km),0) as km,
-			ifnull(sum(vl.consumption),0) as consumed from
-			`tabVehicle Logbook` vl where vl.equipment_number = '{2}' {6} and
-			vl.docstatus = 1 having sum(vl.consumption)>0""".format(eq.name, eq.equipment_type, eq.equipment_number, eq.branch, eq.hsd_type, eq.equipment_model, vl_date, from_date, to_date)
+		query = """ 
+				select 
+					'{0}' as name, '{1}' as ty, '{2}' as no, '{3}' as br, vl.branch, 
+					ifnull(MIN(vl.initial_km),0)  AS mink, ifnull(MAX(vl.final_km),0) AS maxk, 
+					ifnull(MIN(vl.initial_hour),0) as minh, ifnull(MAX(vl.final_hour),0) as maxh, 
+					ifnull(vl.consumption_km,0) as ckm, ifnull(vl.consumption_hours,0) as ch,
+					( select ifnull((sum(pol.qty*pol.rate)/sum(pol.qty)),0) 
+						from tabPOL pol 
+						where pol.equipment_number = '{2}' and pol.posting_date between '{7}' and '{8}'
+					) as rate,
+					(select em.tank_capacity 
+						from  `tabEquipment Model` em 
+						where em.name = '{5}'
+					) as cap, 
+					CASE
+						WHEN vl.ys_km THEN vl.ys_km
+						else 0
+					END as yskm,
+					CASE
+						WHEN vl.ys_hours THEN vl.ys_hours
+						else 0
+					END as yshour,
+					ifnull(sum(vl.distance_km),0) as km, ifnull(sum(vl.consumption),0) as consumed 
+				from
+					`tabVehicle Logbook` vl 
+				where vl.equipment_number = '{2}' {6} and vl.docstatus = 1 having sum(vl.consumption)>0
+			""".format(eq.name, eq.equipment_type, eq.equipment_number, eq.branch, eq.hsd_type, eq.equipment_model, vl_date, from_date, to_date)
+
 		datas = frappe.db.sql(query, as_dict=1)
 		for d in datas:
-                	d.drawn = get_pol_between("Receive", d.name, filters.from_date, filters.to_date, d.hsd_type)
-                	received_till = get_pol_till("Receive", d.name, filters.from_date, d.hsd_type)
-                	consumed_till = get_pol_consumed_till(d.name, filters.from_date)
-                	d.opening = flt(received_till) - flt(consumed_till)
-                	d.closing = flt(d.opening) + flt(d.drawn) - flt(d.consumed)
+			d.drawn = get_pol_between("Receive", d.name, filters.from_date, filters.to_date, d.hsd_type)
+			received_till = get_pol_till("Receive", d.name, filters.from_date, filters.to_date, d.hsd_type)
+			consumed_till = get_pol_consumed_till(d.name, filters.from_date, filters.to_date)
+			d.opening = flt(received_till) - flt(consumed_till)
+			d.closing = flt(d.opening) + flt(d.drawn) - flt(d.consumed)
 
-                	row = [d.name, d.ty, d.no, d.br, ("{0} / {1}".format(d.mink, d.minh)), ("{0} / {1}".format(d.maxk,d.maxh)), round(flt(d.maxk)-flt(d.mink),2), round(flt(d.maxh)-flt(d.minh),2),
-                round(flt(d.drawn),2), round(flt(d.opening),2), round((flt(d.drawn)+flt(d.opening)),2),
-                d.yskm, d.yshour, round(d.consumed,2), round(flt(d.closing),2), flt(d.cap), round(flt(d.rate),2), round((flt(d.rate)*flt(d.consumed)),2)]
-                	data.append(row)
+			row = [d.name, d.ty, d.no, d.br, ("{0} / {1}".format(d.mink, d.minh)), ("{0} / {1}".format(d.maxk,d.maxh)), round(flt(d.maxk)-flt(d.mink),2), round(flt(d.maxh)-flt(d.minh),2),
+			round(flt(d.drawn),2), round(flt(d.opening),2), round((flt(d.drawn)+flt(d.opening)),2),
+			d.yskm, d.yshour, round(d.consumed,2), round(flt(d.closing),2), flt(d.cap), round(flt(d.rate),2), round((flt(d.rate)*flt(d.consumed)),2)]
+			data.append(row)
 	return data
 
 def get_columns():
@@ -105,6 +119,5 @@ def get_columns():
 		("Tank Capacity")+":data:110",
 		("Rate(Nu.)")+":currency:100",
 		("Amount(Nu.)")+":Currency:100",
-
 	]
 	return cols
--- erpnext/maintenance/report/maintenance_report.py
+++ erpnext/maintenance/report/maintenance_report.py
@@ -6,16 +6,24 @@ from frappe.utils import flt, cint,add_days, cstr, flt, getdate, nowdate, rounde
 ##
 # Both recieved and issued pols can be queried with this
 ##
-def get_pol_till(purpose, equipment, posting_date, pol_type=None, own_cc=None, posting_time="23:59:59"):
-	if not equipment or not posting_date:
+def get_pol_till(purpose, equipment, from_date, to_date, pol_type=None, own_cc=None, posting_time="23:59:59"):
+	if not equipment or not from_date or not to_date:
 		frappe.throw("Equipment and Till Date are Mandatory")
+		
 	total = 0
-	posting_datetime = str(get_datetime(str(posting_date) + ' ' + str(posting_time)))
-	query = "select sum(qty) as total from `tabPOL Entry` where docstatus = 1 and type = \'"+str(purpose)+"\' and equipment = \'" + str(equipment) + "\' and cast(concat(date, ' ' , posting_time) as datetime) <= \'" + str(posting_datetime) + "\'"
+	query = """
+			select sum(qty) as total 
+			from `tabPOL Entry` 
+			where docstatus = 1 and type = '{}' and equipment = '{}' 
+			and date >= '{}' 
+			and date <= '{}'
+		""".format(purpose, equipment, from_date, to_date)
+	
 	if pol_type:
 		query += " and pol_type = \'" + str(pol_type) + "\'"
 	if own_cc:
 		query += " and own_cost_center = 1"
+
 	quantity = frappe.db.sql(query, as_dict=True)
 	if quantity:
 		total = quantity[0].total
@@ -42,15 +50,29 @@ def get_pol_between(purpose, equipment, from_date, to_date, pol_type=None, own_c
 ##
 # Get consumed POl as per yardstick
 ##
-def get_pol_consumed_till(equipment, posting_date, posting_time="23:59:59", filter_dry=None):
-	if not equipment or not posting_date:
-                frappe.throw("Equipment and Till Date are Mandatory")
-	posting_datetime = str(get_datetime(str(posting_date) + ' ' + str(posting_time)))
+def get_pol_consumed_till(equipment, from_date, to_date, posting_time="23:59:59", filter_dry=None):
+	if not equipment or not from_date or not to_date:
+		frappe.throw("Equipment and Till Date are Mandatory")
 
 	if not filter_dry:
-		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and equipment = %s and cast(concat(to_date, ' ', to_time) as datetime) <= %s", (equipment, str(posting_datetime)), as_dict=True)
+		query = """	
+				select sum(consumption) as total 
+				from `tabVehicle Logbook` 
+				where docstatus = 1 and equipment = '{}' 
+				and from_date >= '{}' 
+				and to_date <= '{}'
+		""".format(equipment, from_date, to_date)
 	else:
-		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and equipment = %s and rate_type = 'With Fuel' and cast(concat(to_date, ' ', to_time)  as datetime) <= %s", (equipment, str(posting_datetime) ), as_dict=True)
+		query = """
+				select 
+					sum(consumption) as total 
+				from `tabVehicle Logbook` 
+				where docstatus = 1 and equipment = '{}' and rate_type = 'With Fuel' 
+				and from_date >= '{}' 
+				and to_date <= '{}'
+		""".format(equipment, from_date, to_date)
+	
+	pol = frappe.db.sql(query, as_dict=True)
 	if pol:
 		return pol[0].total
 	else:
@@ -58,7 +80,8 @@ def get_pol_consumed_till(equipment, posting_date, posting_time="23:59:59", filt
 
 def get_km_till(equipment, date):
 	if not equipment or not date:
-                frappe.throw("Equipment and Till Date are Mandatory")
+		frappe.throw("Equipment and Till Date are Mandatory")
+	
 	km = frappe.db.sql("select final_km from `tabVehicle Logbook` where docstatus = 1 and equipment = %s and to_date <= %s order by final_km desc limit 1", (equipment, date), as_dict=True)
 	if km:
 		return km[0].final_km
