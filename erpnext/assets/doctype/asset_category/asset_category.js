a9f2d7f4f20203fe254a1ae375301b0f0ad02bb6
chophel9808 <sonamax809@gmail.com>
2022-01-10 15:50:34 +0600

    Show all accounts rather than showing only fixed asset accounts

--- erpnext/assets/doctype/asset_category/asset_category.js
+++ erpnext/assets/doctype/asset_category/asset_category.js
@@ -15,17 +15,17 @@ frappe.ui.form.on('Asset Category', {
 		frm.add_fetch('company_name', 'accumulated_depreciation_account', 'accumulated_depreciation_account');
 		frm.add_fetch('company_name', 'depreciation_expense_account', 'accumulated_depreciation_account');
 
-		frm.set_query('fixed_asset_account', 'accounts', function (doc, cdt, cdn) {
-			var d = locals[cdt][cdn];
-			return {
-				"filters": {
-					"account_type": "Fixed Asset",
-					"root_type": "Asset",
-					"is_group": 0,
-					"company": d.company_name
-				}
-			};
-		});
+		// frm.set_query('fixed_asset_account', 'accounts', function (doc, cdt, cdn) {
+		// 	var d = locals[cdt][cdn];
+		// 	return {
+		// 		"filters": {
+		// 			"account_type": "Fixed Asset",
+		// 			"root_type": "Asset",
+		// 			"is_group": 0,
+		// 			"company": d.company_name
+		// 		}
+		// 	};
+		// });
 
 		frm.set_query('accumulated_depreciation_account', 'accounts', function (doc, cdt, cdn) {
 			var d = locals[cdt][cdn];
