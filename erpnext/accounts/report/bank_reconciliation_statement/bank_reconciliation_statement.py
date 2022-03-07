6e30ccea635e3ebf405075e5a4c7b177802a595c
Kinley Tshering <kxt5258@gmail.com>
2022-03-07 17:22:54 +0600

    BRS and Bank Reconciliation, TDS Remittance included

--- erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
+++ erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
@@ -228,22 +228,34 @@ def get_entries(filters):
 		and ifnull(clearance_date, '4000-01-01') > %(report_date)s
 	""", filters, as_dict=1)
 
-        direct_payment_entries = frappe.db.sql("""
-                        select
-                                "Direct Payment" as payment_document, name as payment_entry,
-                                cheque_no as reference_no, cheque_date as ref_date,
-                                net_amount as credit, 0 as debit,
-                                posting_date, branch as against_account, clearance_date
-                        from `tabDirect Payment`
-                        where %(account)s IN (credit_account, debit_account)
-                        and docstatus = 1
-                        and posting_date <= %(report_date)s
-                        and ifnull(clearance_date, '4000-01-01') > %(report_date)s
-                """, filters, as_dict=1)
+	direct_payment_entries = frappe.db.sql("""
+					select
+							"Direct Payment" as payment_document, name as payment_entry,
+							cheque_no as reference_no, cheque_date as ref_date,
+							net_amount as credit, 0 as debit,
+							posting_date, branch as against_account, clearance_date
+					from `tabDirect Payment`
+					where %(account)s IN (credit_account, debit_account)
+					and docstatus = 1
+					and posting_date <= %(report_date)s
+					and ifnull(clearance_date, '4000-01-01') > %(report_date)s
+			""", filters, as_dict=1)
 
+	tds_remittance_entries = frappe.db.sql ("""
+		select
+			"TDS Remittance" as payment_document, name as payment_entry,
+			cheque_no as reference_no, cheque_date as ref_date,
+			total_tds as credit, 0 as debit,
+			posting_date, branch as against_account, clearance_date
+		from `tabTDS Remittance`
+		where account = %(account)s
+		and docstatus =1
+		and posting_date <= %(report_date)s
+		and ifnull(clearance_date, '4000-01-01') > %(report_date)s
+	""",filters, as_dict=1)
 
-        return sorted(list(payment_entries)+list(journal_entries)+list(hsd_entries)+list(imprest_entries)+list(mechanical_entries)+list(project_entries)+list(direct_payment_entries),
-		key=lambda k: k['posting_date'] or getdate(nowdate()))
+	return sorted(list(payment_entries)+list(journal_entries)+list(hsd_entries)+list(imprest_entries)+list(mechanical_entries)+list(project_entries)+list(direct_payment_entries)+list(tds_remittance_entries),
+	key=lambda k: k['posting_date'] or getdate(nowdate()))
 
 		
 def get_amounts_not_reflected_in_system(filters):
