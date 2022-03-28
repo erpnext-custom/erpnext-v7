dcda3c9b22917ce28d3422ae8e32b82d90a98963
kinleydorji <kd77144@gmail.com>
2022-03-28 12:12:04 +0600

    equipment performance report

--- erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
+++ erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
@@ -486,29 +486,29 @@ def get_data(filters):
 		if not benchmark:
 			benchmark = {"rat": 0, "bn": 0, "fr": '', "t": ''}
 		#ben = [i*8 for i in bench]
-                # utility % based on existance of revenue and benchmark target'''
+		# utility % based on existance of revenue and benchmark target'''
 
 		util_percent = 0
 		if total_work_time == None:
 			total_work_time = 0
-                # if total_hc != 0.0:
+		# if total_hc != 0.0:
 		if total_work_time != 0:
-            util_percent = 100*(total_work_time/benchm)
+			util_percent = 100*(total_work_time/benchm)
 		else:
 			util_percent = 0
-			#frappe.msgprint("1 rev : {0}, HC: {1} per: {2}".format(total_rev, total_hc, util_percent))
+		#frappe.msgprint("1 rev : {0}, HC: {1} per: {2}".format(total_rev, total_hc, util_percent))
 
-            #     elif flt(total_hc) == 0  and flt(total_rev) > 0:
-            #             util_percent = 100
-			# #frappe.msgprint("2 {0} ".format(util_percent))
+		#     elif flt(total_hc) == 0  and flt(total_rev) > 0:
+		#             util_percent = 100
+		# #frappe.msgprint("2 {0} ".format(util_percent))
 
-            #     elif flt(total_hc) == 0.0 and flt(total_rev) <= 0.0:
-            #             util_percent = 0.0
-			# #frappe.msgprint("3 {0}".format(util_percent))
+		#     elif flt(total_hc) == 0.0 and flt(total_rev) <= 0.0:
+		#             util_percent = 0.0
+		# #frappe.msgprint("3 {0}".format(util_percent))
 
-            #     else:
-            #             util_percent = 0.0
-			#frappe.msgprint("4 {0}".format(util_percent))
+		#     else:
+		#             util_percent = 0.0
+		#frappe.msgprint("4 {0}".format(util_percent))
 
 		#frappe.msgprint(_("{0}, {1}, {2}").format(total_rev, total_hc, util_percent))
 
