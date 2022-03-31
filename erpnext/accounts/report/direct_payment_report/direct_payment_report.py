231720d11936226426bab072ed588ddc52cfa353
Kinley Tshering <kxt5258@gmail.com>
2022-03-31 17:35:12 +0600

    Modified to show the party amounts

--- erpnext/accounts/report/direct_payment_report/direct_payment_report.py
+++ erpnext/accounts/report/direct_payment_report/direct_payment_report.py
@@ -11,13 +11,29 @@ def execute(filters=None):
 
 
 def get_data(filters):
-	query = """ select posting_date, name, party_type, party, cost_center, payment_type, debit_account, amount, taxable_amount, tds_amount, net_amount from `tabDirect Payment` where docstatus = 1"""
+	'''query = """ select posting_date, name, party_type, party, cost_center, single_party_multiple_payments, payment_type, debit_account, amount, taxable_amount, tds_amount, net_amount from `tabDirect Payment` where docstatus = 1"""
+	'''
+	##replaced with following query by Tashi
+	query = """
+		SELECT
+			dp.posting_date, dp.name, 
+			CASE dp.single_party_multiple_payments WHEN 1 THEN 'Supplier' ELSE ifnull(dpi.party_type, dp.party_type) END AS party_type,
+			ifnull(dpi.party, dp.party) as party,
+			dp.cost_center, dp.payment_type, dp.debit_account,  ifnull(dpi.amount, dp.amount) as amount, 
+			IFNULL(dpi.taxable_amount, dp.taxable_amount) as taxable_amount,
+			IFNULL(dpi.tds_amount, dp.tds_amount) as tds_amount,
+			IFNULL(dpi.net_amount, dp.net_amount) as net_amount
+		FROM 
+			`tabDirect Payment` dp 
+		LEFT JOIN  
+			`tabDirect Payment Item` dpi 
+		ON dpi.parent = dp.name WHERE dp.docstatus = 1
+	"""
 	if filters.get("branch"):
-		query += " and branch = '{0}'".format(filters.get("branch"))
+		query += " and dp.branch = '{0}'".format(filters.get("branch"))
 	if filters.get("from_date") and filters.get("to_date"):
-		query += " and posting_date between '{0}' and '{1}'".format(filters.get("from_date"), filters.get("to_date"))
-	
-	return frappe.db.sql(query) 
+		query += " and dp.posting_date between '{0}' and '{1}'".format(filters.get("from_date"), filters.get("to_date"))
+	return frappe.db.sql(query, debug =1) 
 
 def get_columns():
 	return [
