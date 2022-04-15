7d6afdab173b44235dc2534579cee596b1329e17
kinleydorji <kd77144@gmail.com>
2022-04-15 18:29:06 +0600

    overall sales reporat

--- erpnext/selling/report/overall_sales_report/overall_sales_report.py
+++ erpnext/selling/report/overall_sales_report/overall_sales_report.py
@@ -243,7 +243,7 @@ def get_data(filters=None):
 					WHEN dni.conversion_req=1 THEN dni.sales_uom
 					ELSE dni.stock_uom
 				END as uom, sum(dni.amount),
-				sum(dni.amount) - (dni.discount_or_cost_amount) + sum(dni.additional_cost) - sum(dn.challan_cost)
+				sum(dni.amount) - (dni.discount_amount) + sum(dni.additional_cost) - sum(dn.challan_cost)
 			"""
 			group_by = " group by dn.branch, dn.location, i.item_sub_group"
 			order_by = ""
