a5427b7367009b75d7e2f9f66236999b401f2e02
Kinley Tshering <kxt5258@gmail.com>
2022-04-27 11:38:25 +0600

    MPI FOR 2021

--- erpnext/hr/hr_custom_patch.py
+++ erpnext/hr/hr_custom_patch.py
@@ -7,6 +7,41 @@ from frappe.utils import flt, cint
 from frappe.utils.data import get_first_day, get_last_day, add_days
 from erpnext.custom_utils import get_year_start_date, get_year_end_date
 
+# following method created by SHIV on 2022/04/27
+def mpi_2021():
+	li = frappe.db.sql("""
+			SELECT m.employee, ss.name salary_structure, m.amount
+			FROM mpi_2021 m, `tabEmployee` e, `tabSalary Structure` ss
+			WHERE e.name = m.employee
+			AND ss.employee = e.name
+			AND ss.is_active = 'Yes'
+			AND NOT EXISTS(SELECT 1
+					FROM `tabSalary Detail` sd
+					WHERE sd.parent = ss.name
+					AND sd.salary_component = 'Salary Arrears'
+					AND sd.reference_number != 'MPIFOR2021'
+					)
+			""", as_dict=True)
+	counter = 0
+	for i in li:
+		counter += 1
+		print(counter,i.employee, i.salary_structure, i.amount)
+		if flt(i.amount) <= 0:
+			frappe.throw(_("ERROR: Invalid Amount"))
+
+		# continue
+		sst = frappe.get_doc("Salary Structure", i.salary_structure)
+		row = sst.append("earnings", {})
+		row.salary_component 	= "Salary Arrears"
+		row.salary_component_type = "Earning"
+		row.amount 				= flt(i.amount)
+		row.from_date 			= "2022-04-01"
+		row.to_date 			= "2022-04-30"
+		row.reference_number 	= "MPIFOR2021"
+		row.save(ignore_permissions=True)
+		sst.save(ignore_permissions=True)
+	frappe.db.commit()
+
 # following method created by SHIV on 2021/05/27
 def refresh_salary_structures():
 	counter = 0
