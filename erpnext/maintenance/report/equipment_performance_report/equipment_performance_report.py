88b727f082ae2d8061e5efac257199dc2c00055f
kinleydorji <kd77144@gmail.com>
2022-05-25 13:50:46 +0600

    equipmpent performance report: added total revenue field for own equipment report

--- erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
+++ erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
@@ -374,7 +374,7 @@ def get_data(filters):
 		# if eq.equipment_number == "BG-2-A0695":
 		# 	frappe.msgprint(str(travel_claim)+" "+str(e_amount)+" "+str(gross_pay))
 		#benchmark
-		query = frappe.db.sql("""
+		query = """
 							   select hi.rate_fuel as rat, hi.perf_bench as bn, hi.cft_rate_bf as cft_rate_bf, hi.cft_rate_co as cft_rate_co,
 							   hi.from_date as fr, hi.to_date as t
 							   from  `tabHire Charge Item` hi, `tabHire Charge Parameter` hp
