f80e30b60623aad59415e30beab489b798d8bc43
kinleydorji <kd77144@gmail.com>
2022-05-25 13:53:10 +0600

    equipmpent performance report: added total revenue field for own equipment report

--- erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
+++ erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
@@ -374,15 +374,6 @@ def get_data(filters):
 		# if eq.equipment_number == "BG-2-A0695":
 		# 	frappe.msgprint(str(travel_claim)+" "+str(e_amount)+" "+str(gross_pay))
 		#benchmark
-		query = """
-							   select hi.rate_fuel as rat, hi.perf_bench as bn, hi.cft_rate_bf as cft_rate_bf, hi.cft_rate_co as cft_rate_co,
-							   hi.from_date as fr, hi.to_date as t
-							   from  `tabHire Charge Item` hi, `tabHire Charge Parameter` hp
-							   where hi.parent = hp.name 
-							   and hp.equipment_type = '{0}'
-				   and hp.equipment_model = '{1}'
-				   and '{2}' >= hi.from_date and  '{2}' <= hi.to_date and '{3}' >= hi.from_date and '{3}' <= hi.to_date
-			""".format(eq.equipment_type, eq.equipment_model, from_date, to_date)
 		benchmark  = frappe.db.sql("""
 							   select hi.rate_fuel as rat, hi.perf_bench as bn, hi.cft_rate_bf as cft_rate_bf, hi.cft_rate_co as cft_rate_co,
 							   hi.from_date as fr, hi.to_date as t
@@ -390,10 +381,8 @@ def get_data(filters):
 							   where hi.parent = hp.name 
 							   and hp.equipment_type = '{0}'
 				   and hp.equipment_model = '{1}'
-				   and '{2}' >= hi.from_date and  '{2}' <= hi.to_date and '{3}' >= hi.from_date and '{3}' <= hi.to_date
+				   and hi.from_date between '{2}' and '{3}' and hi.to_date between '{2}' and '{3}'
 			""".format(eq.equipment_type, eq.equipment_model, from_date, to_date), as_dict=1)
-		if frappe.session.user == "Administrator":
-			frappe.msgprint(str(query))
 			# frappe.msgprint(str(eq.equipment_type)+" "+str(eq.equipment_model)+" "+str(from_date)+" "+str(to_date))
 		rate = []
 		bench = []
