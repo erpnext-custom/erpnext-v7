90c5f24f880cc2f523087dac7ec4c4d041467a98
kinleydorji <kd77144@gmail.com>
2022-05-25 13:44:43 +0600

    equipmpent performance report: added total revenue field for own equipment report

--- erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
+++ erpnext/maintenance/report/equipment_performance_report/equipment_performance_report.py
@@ -383,8 +383,8 @@ def get_data(filters):
 				   and hp.equipment_model = '{1}'
 				   and '{2}' >= hi.from_date and  '{2}' <= hi.to_date and '{3}' >= hi.from_date and '{3}' <= hi.to_date
 			""".format(eq.equipment_type, eq.equipment_model, from_date, to_date), as_dict=1)
-		# if frappe.session.user == "Administrator":
-		# 	frappe.msgprint(str(benchmark))
+		if frappe.session.user == "Administrator":
+			frappe.msgprint(str(benchmark))
 		rate = []
 		bench = []
 		total_hc = 0
