95fc002286e211cfa31756394a3b0f41286a6e74
Jai <jairajrai1@gmail.com>
2022-07-13 15:47:08 +0600

    custom_utils, check_budget_availability

--- erpnext/custom_utils.py
+++ erpnext/custom_utils.py
@@ -213,7 +213,10 @@ def prepare_gl(d, args):
 def check_budget_available(cost_center, budget_account, transaction_date, amount, business_activity):
 	if frappe.db.get_value("Account", budget_account, "budget_check"):
                 return
-        fiscal_year = frappe.db.sql("select name from `tabFiscal Year` where '{}' between year_start_date and year_end_date and disabled = 0".format(str(transaction_date)))[0][0]
+        fiscal_year = frappe.db.sql("select name, year_start_date, year_end_date from `tabFiscal Year` where '{}' between year_start_date and year_end_date and disabled = 0".format(str(transaction_date)), as_dict=True)
+        if not fiscal_year:
+                frappe.throw(_("Transaction date {} does not fall under any of the <b>Fiscal Year</b>").format(str(transaction_date)), title="Budget Check Failed")
+
         budget_amount = frappe.db.sql("""select b.action_if_annual_budget_exceeded as action, ba.budget_check, ba.budget_amount 
                                         from 
                                                 `tabBudget` b,
@@ -224,7 +227,7 @@ def check_budget_available(cost_center, budget_account, transaction_date, amount
                                                 and b.cost_center=%s 
                                                 and b.fiscal_year = %s
                                                 and b.business_activity = %s
-                                """, (budget_account, cost_center, fiscal_year, business_activity), as_dict=True)
+                                """, (budget_account, cost_center, fiscal_year[0]["name"], business_activity), as_dict=True)
         #action = frappe.db.sql("select action_if_annual_budget_exceeded as action from tabBudget where docstatus = 1 and cost_center = \'" + str(cost_center) + "\' and fiscal_year = " + str(transaction_date)[0:4] + " ", as_dict=True)
 	ig_or_stop = budget_amount and budget_amount[0].action or None
         ig_or_stop_gl = budget_amount and budget_amount[0].budget_check or None
@@ -232,8 +235,8 @@ def check_budget_available(cost_center, budget_account, transaction_date, amount
                 return
         else:
                 if budget_amount:
-                        committed = frappe.db.sql("select SUM(cb.amount) as total from `tabCommitted Budget` cb where cb.cost_center=%s and cb.account=%s and cb.po_date between %s and %s and cb.business_activity =%s", (cost_center, budget_account, str(transaction_date)[0:4] + "-01-01", str(transaction_date)[0:4] + "-12-31", business_activity), as_dict=True)
-                        consumed = frappe.db.sql("select SUM(cb.amount) as total from `tabConsumed Budget` cb where cb.cost_center=%s and cb.account=%s and cb.po_date between %s and %s and cb.business_activity =%s", (cost_center, budget_account, str(transaction_date)[0:4] + "-01-01", str(transaction_date)[0:4] + "-12-31", business_activity), as_dict=True)
+                        committed = frappe.db.sql("select SUM(cb.amount) as total from `tabCommitted Budget` cb where cb.cost_center=%s and cb.account=%s and cb.po_date between %s and %s and cb.business_activity =%s", (cost_center, budget_account, fiscal_year[0]["year_start_date"], fiscal_year[0]["year_end_date"], business_activity), as_dict=True)
+                        consumed = frappe.db.sql("select SUM(cb.amount) as total from `tabConsumed Budget` cb where cb.cost_center=%s and cb.account=%s and cb.po_date between %s and %s and cb.business_activity =%s", (cost_center, budget_account, fiscal_year[0]["year_start_date"], fiscal_year[0]["year_end_date"], business_activity), as_dict=True)
 			if consumed and committed:
 				if consumed[0].total > committed[0].total:
 					committed = consumed
