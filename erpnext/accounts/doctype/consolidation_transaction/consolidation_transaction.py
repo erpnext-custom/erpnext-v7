8e379600d271e5a1a390d2a391c88c5ca3895c4e
BIREN <>
2022-10-16 21:15:58 +0600

    consolidation changes applied

--- erpnext/accounts/doctype/consolidation_transaction/consolidation_transaction.py
+++ erpnext/accounts/doctype/consolidation_transaction/consolidation_transaction.py
@@ -5,6 +5,20 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class ConsolidationTransaction(Document):
 	pass
+
+@frappe.whitelist()
+def make_adjustment_entry(source_name, target_doc=None):
+        
+        doclist = get_mapped_doc("Consolidation Transaction", source_name, {
+                "Consolidation Transaction": {
+						"doctype": "Consolidation Adjustment Entry",
+						"field_map":{
+								"name": "consolidation_transaction"
+						},
+                        }
+        }, target_doc)
+        return doclist
--- erpnext/accounts/report/dhi_gcoa_mapper_report/dhi_gcoa_mapper_report.py
+++ erpnext/accounts/report/dhi_gcoa_mapper_report/dhi_gcoa_mapper_report.py
@@ -9,14 +9,12 @@ def execute(filters=None):
 	return columns, data
 
 def get_data(filters):
-	cond = ''
+	cond = ' where 1 = 1 '
 	data = []
 	if filters.is_inter_company:
-		cond += " where is_inter_company = 1 "
+		cond += " and is_inter_company = 1 "
 	if filters.dhi_gcoa_acc:
-		cond += " where account_code = '{}' ".format(filters.dhi_gcoa_acc)
-	if filters.is_inter_company and filters.dhi_gcoa_acc:
-		cond = " where where is_inter_company = 1 and account_code = '{}' ".format(filters.dhi_gcoa_acc)
+		cond += " and account_code = '{}' ".format(filters.dhi_gcoa_acc)
 	
 	if not filters.map or filters.map.strip() == "GCOA Mapped":
 		for d in frappe.db.sql('''
@@ -38,22 +36,26 @@ def get_data(filters):
 					   '''.format(d.dhi_gcoa_acc,d.dhi_gcoa_acc_code,d.is_inter_company,d.dhi_gcoa_acc),as_dict=1)
 			data += val
 	elif filters.map.strip() == "COA Unmapped":
+		if filters.is_inter_company:
+			cond = " and b.is_inter_company = 1 "
+		else:
+			cond = " and b.is_inter_company = 0 "
 		data = frappe.db.sql('''
 						SELECT name as coa_acc
 						FROM `tabAccount` a
-						WHERE NOT EXISTS (	select 1 from `tabDHI Mapper Item` where account = a.name)
+						WHERE NOT EXISTS (	select 1 from `tabDHI GCOA Mapper` b, `tabDHI Mapper Item` c where c.account = a.name {})
 						AND a.is_group != 1
-						 ''',as_dict=1)
+						 '''.format(cond),as_dict=1)
 	return data
 	
 def get_cols(filters):
 
 	if not filters.map or filters.map.strip() == "GCOA Mapped":
-			cols = [{"fieldname":"dhi_gcoa_acc_code","label":"DHI GCOA Code","fieldtype":"Link","options":"DHI GCOA","width":120,"bold":1},
-					{"fieldname":"dhi_gcoa_acc","label":"DHI Group Chart Of Account","fieldtype":"Link","options":"DHI GCOA Mapper","width":250,"bold":1},
-					{"fieldname":"is_inter_company","label":"Is Inter Company","fieldtype":"Data","width":150},
-					{"fieldname":"coa_acc","label":"Company Chart Of Account","fieldtype":"Link","options":"Account","width":400}
-				]
+			cols = [{"fieldname":"dhi_gcoa_acc_code","label":"DHI GCOA Code","fieldtype":"Link","options":"DHI GCOA","width":120,},
+		{"fieldname":"dhi_gcoa_acc","label":"DHI Group Chart Of Account","fieldtype":"Link","options":"DHI GCOA Mapper","width":250,},
+		{"fieldname":"is_inter_company","label":"Is Inter Company","fieldtype":"Data","width":150},
+		{"fieldname":"coa_acc","label":"Company Chart Of Account","fieldtype":"Link","options":"Account","width":400}
+	]
 	elif filters.map.strip() == "COA Unmapped":
 		cols = [{"fieldname":"coa_acc","label":"Company Chart Of Account","fieldtype":"Link","options":"Account","width":400}]
 	return cols
