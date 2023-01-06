0c5c9af3257f7fe6c11ab38f479e39ab81083f59
kinleydorji <kd77144@gmail.com>
2023-01-06 12:02:07 +0600

    selling price

--- erpnext/production/doctype/selling_price/selling_price.js
+++ erpnext/production/doctype/selling_price/selling_price.js
@@ -2,14 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Selling Price', {
-	setup: function(frm) {
-		frm.get_field('item_rates').grid.editable_fields = [
-			// {fieldname: 'price_based_on', columns: 3},
-			{fieldname: 'particular', columns: 3},
-			{fieldname: 'selling_price', columns: 3},
-			{fieldname: 'item_sub_group', columns:3},
-		]
-	},
+	// setup: function(frm) {
+	// 	frm.get_field('item_rates').grid.editable_fields = [
+	// 		// {fieldname: 'price_based_on', columns: 3},
+	// 		{fieldname: 'particular', columns: 3},
+	// 		{fieldname: 'selling_price', columns: 3},
+	// 		{fieldname: 'item_sub_group', columns:3},
+	// 	]
+	// },
 
 	refresh: function(frm) {
 
