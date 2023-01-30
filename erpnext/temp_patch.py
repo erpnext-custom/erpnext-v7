c4604d82046b84f1ec58b8101bf8988c807320d1
kalisetti <sivasankar.k2003@gmail.com>
2023-01-30 23:34:00 +0600

    Submit Stock Reconciliation entries

--- erpnext/temp_patch.py
+++ erpnext/temp_patch.py
@@ -9,7 +9,23 @@ from erpnext.hr.hr_custom_functions import get_month_details, get_salary_tax
 import collections
 from frappe.model.naming import make_autoname
 
-
+def submit_sr20230128(submit=0):
+        counter = 0
+        for i in frappe.db.sql("""select name from `tabStock Reconciliation`
+                where name in ('SR/000905','SR/000887','SR/000883','SR/000882',
+                        'SR/000881','SR/000749','SR/000744','SR/000669')
+                        order by name""", as_dict=True):
+                counter += 1
+                doc = frappe.get_doc('Stock Reconciliation', i.name)
+                print(counter, doc.name, doc.docstatus)
+                if submit and doc.docstatus == 0:
+                        try:
+                                doc.submit()
+                                print 'Submitted successfully...'
+                        except Exception, e:
+                                print 'ERROR: ', str(e)
+                frappe.db.commit()
+                
 def cancel_sr20230128(cancel=0):
         counter = 0
         for i in frappe.db.sql("""select name from `tabStock Reconciliation`
