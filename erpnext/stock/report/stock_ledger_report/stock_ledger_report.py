c45f2eaae7b5d00685b2fceeb69bdb57f76e767c
kalisetti <sivasankar.k2003@gmail.com>
2023-02-13 10:55:57 +0600

    Stock Ledger Report: Bugfixes

--- erpnext/stock/report/stock_ledger_report/stock_ledger_report.py
+++ erpnext/stock/report/stock_ledger_report/stock_ledger_report.py
@@ -58,7 +58,7 @@ def get_stock_ledger_entries(filters):
     	                            WHEN sle.voucher_type = 'Production' THEN prod.branch
     	                            WHEN sle.voucher_type = 'Purchase Receipt' THEN pr.branch
     	                            ELSE 'None' END) as branch, 
-				sle.warehouse, sum(sle.actual_qty) as actual_qty, (sle.qty_after_transaction) as qty_after_transaction, sle.incoming_rate, sle.valuation_rate,
+				sle.warehouse, sle.actual_qty as actual_qty, sle.qty_after_transaction as qty_after_transaction, sle.incoming_rate, sle.valuation_rate,
 				sle.stock_value, sle.stock_value_difference,
 				(CASE
     	                            WHEN sle.voucher_type = 'Production' AND pmi.name = sle.voucher_detail_no THEN 'Raw Materials'
@@ -95,7 +95,7 @@ def get_stock_ledger_entries(filters):
     	                            WHEN sle.voucher_type = 'Production' THEN prod.branch
     	                            WHEN sle.voucher_type = 'Purchase Receipt' THEN pr.branch
     	                            ELSE 'None' END) as branch, 
-				sle.warehouse, sum(sle.actual_qty) as actual_qty, (sle.qty_after_transaction) as qty_after_transaction, sle.incoming_rate, sle.valuation_rate,
+				sle.warehouse, sle.actual_qty as actual_qty, sle.qty_after_transaction as qty_after_transaction, sle.incoming_rate, sle.valuation_rate,
 				sle.stock_value, sle.stock_value_difference,
 				(CASE
     	                            WHEN sle.voucher_type = 'Production' AND pmi.name = sle.voucher_detail_no THEN 'Raw Materials'
