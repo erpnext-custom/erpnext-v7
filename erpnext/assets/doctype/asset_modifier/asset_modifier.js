3cfb1b79f1e46726e2f167e6590837bf9ca4b06d
Jai Raj Rai <jairajrai1@gmail.com>
2023-02-14 11:01:54 +0600

    asset_modifier.js bulk_edit enable in items

--- erpnext/assets/doctype/asset_modifier/asset_modifier.js
+++ erpnext/assets/doctype/asset_modifier/asset_modifier.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Modifier', {
+	setup: function(frm) {
+		frm.get_docfield("items").allow_bulk_edit = 1;
+	},
 	refresh: function(frm) {
 
 	},
